9cb1ec207fed1dfa476ff2dc0ab1b27e5d5f6e42
BoThe1K <bohdan@thestratos.org>
2025-02-26 20:51:15 +0100

    Update sds api

--- core/node/core.go
+++ core/node/core.go
@@ -246,7 +246,6 @@ func NamespaceFiles(cfg *config.Config, mctx helpers.MetricsCtx, lc fx.Lifecycle
 	}
 
 	dsk := datastore.NewKey("/local/filesroot")
-	offineDag := merkledag.NewDAGService(blockservice.New(bs, offline.Exchange(bs)))
 	dsDag := shockdag.NewDsDagService(mfsDS, func(c cid.Cid) datastore.Key {
 		return shockds.JoinKeys(dsk, datastore.NewKey(fmt.Sprintf("blocks/%s", c.String())))
 	})
@@ -273,13 +272,9 @@ func NamespaceFiles(cfg *config.Config, mctx helpers.MetricsCtx, lc fx.Lifecycle
 				return nil, err
 			}
 
-			// legacy support
-			rnd, err := offineDag.Get(ctx, c)
+			rnd, err := dsDag.Get(ctx, c)
 			if err != nil {
-				rnd, err = dsDag.Get(ctx, c)
-				if err != nil {
-					return nil, fmt.Errorf("error loading filesroot from shock dag: %s", err)
-				}
+				return nil, fmt.Errorf("error loading filesroot from shock dag: %s", err)
 			}
 
 			pbnd, ok := rnd.(*merkledag.ProtoNode)
