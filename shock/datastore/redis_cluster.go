814786e3f4427f7ee4cffbbdb06893567ac753b4
BoThe1K <bohdan@thestratos.org>
2025-02-27 17:31:20 +0100

    Fix redis cluster get

--- shock/datastore/redis_cluster.go
+++ shock/datastore/redis_cluster.go
@@ -9,9 +9,14 @@ import (
 
 	ds "github.com/ipfs/go-datastore"
 	query "github.com/ipfs/go-datastore/query"
+	logging "github.com/ipfs/go-log/v2"
 )
 
-var _ ds.Datastore = (*redisClusterDatastore)(nil)
+var (
+	_ ds.Datastore = (*redisClusterDatastore)(nil)
+
+	logger = logging.Logger("shock/datastore")
+)
 
 type redisClusterDatastore struct {
 	rdb *redis.ClusterClient
@@ -33,14 +38,18 @@ func NewRedisDatastore(opts *redis.ClusterOptions) (ds.Datastore, error) {
 }
 
 func (d *redisClusterDatastore) convKey(key ds.Key) string {
-	return strings.Join(key.List(), ":")
+	res := strings.Join(key.List(), ":")
+	logger.Debugf("RC key converted: %s", res)
+	return res
 }
 
 func (d *redisClusterDatastore) Close() error {
+	logger.Debugf("RC close")
 	return d.rdb.Close()
 }
 
 func (d *redisClusterDatastore) Delete(ctx context.Context, key ds.Key) error {
+	logger.Debugf("RC delet: %s", key)
 	_, err := d.rdb.Del(ctx, d.convKey(key)).Result()
 	if err != nil {
 		return err
@@ -49,40 +58,41 @@ func (d *redisClusterDatastore) Delete(ctx context.Context, key ds.Key) error {
 }
 
 func (d *redisClusterDatastore) Get(ctx context.Context, key ds.Key) ([]byte, error) {
-	value, err := d.rdb.Get(ctx, d.convKey(key)).Result()
-	switch {
-	case err == redis.Nil:
-		return nil, nil
-	case err != nil:
+	logger.Debugf("RC get: %s", key)
+	value, err := d.rdb.Get(ctx, d.convKey(key)).Bytes()
+	if err != nil {
+		if err == redis.Nil {
+			return nil, ds.ErrNotFound
+		}
 		return nil, err
-	case value == "":
-		return nil, nil
 	}
-	return []byte(value), nil
+	return value, nil
 }
 
 func (d *redisClusterDatastore) GetSize(ctx context.Context, key ds.Key) (int, error) {
+	logger.Debugf("RC get size: %s", key)
 	r, err := d.Get(ctx, key)
 	if err != nil {
-		return 0, err
+		return -1, err
 	}
 	return len(r), nil
 }
 
 func (d *redisClusterDatastore) Has(ctx context.Context, key ds.Key) (bool, error) {
+	logger.Debugf("RC exists: %s", key)
 	res, err := d.rdb.Exists(ctx, d.convKey(key)).Result()
-	switch {
-	case err == redis.Nil:
-		return false, nil
-	case err != nil:
+
+	if err != nil {
+		if err == redis.Nil {
+			return false, ds.ErrNotFound
+		}
 		return false, err
-	case res == 0:
-		return false, nil
 	}
-	return true, nil
+	return res == 1, nil
 }
 
 func (d *redisClusterDatastore) Put(ctx context.Context, key ds.Key, value []byte) error {
+	logger.Debugf("RC put: %s - len: %d", key, len(value))
 	_, err := d.rdb.Set(ctx, d.convKey(key), value, 0).Result()
 	if err != nil {
 		return err
@@ -91,9 +101,11 @@ func (d *redisClusterDatastore) Put(ctx context.Context, key ds.Key, value []byt
 }
 
 func (d *redisClusterDatastore) Query(ctx context.Context, q query.Query) (query.Results, error) {
+	logger.Debugf("RC query: %s", q)
 	return nil, fmt.Errorf("not implemented")
 }
 
 func (d *redisClusterDatastore) Sync(ctx context.Context, prefix ds.Key) error {
+	logger.Debugf("RC sync: %s", prefix)
 	return nil
 }
